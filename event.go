2f66a8d9cca02ebb4d57fda42abb84ce09ec5627
Oppliger J. <account@oppliger.cc>
2026-02-04 17:23:59 +0100

    feat: add events pub/sub

---
+++ event.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"net/http"
+	"fmt"
+	"sync"
+)
+
+type EventType int
+
+const (
+	EntityCreated EventType = iota
+	EntityDeleted
+	EntityUpdated
+	EdgeCreated
+	EdgeDeleted
+	EdgeUpdated
+	EdgeTagCreated
+	EdgeTagDeleted
+	EdgeTagUpdated
+	EntityTagCreated
+	EntityTagDeleted
+	EntityTagUpdated
+)
+
+var eventName = map[EventType]string{
+	EntityCreated: "EntityCreated",
+	EntityDeleted: "EntityDeleted",
+	EntityUpdated: "EntityUpdated",
+	EdgeCreated: "EdgeCreated",
+	EdgeDeleted: "EdgeDeleted",
+	EdgeUpdated: "EdgeUpdated",
+	EdgeTagCreated: "EdgeTagCreated",
+	EdgeTagDeleted: "EdgeTagDeleted",
+	EdgeTagUpdated: "EdgeTagUpdated",
+	EntityTagCreated: "EntityTagCreated",
+	EntityTagDeleted: "EntityTagDeleted",
+	EntityTagUpdated: "EntityTagUpdated",
+}
+
+type ServerSentEvent struct {
+	Event EventType
+	Data []byte
+}
+
+func (sse ServerSentEvent) Write(w http.ResponseWriter) {
+	fmt.Fprintf(w, "event: %s\ndata: %s\n\n", eventName[sse.Event], sse.Data)
+}
+
+type EventBus struct {
+	subscribers map[chan ServerSentEvent]bool
+	mutex       sync.Mutex
+}
+
+func (bus *EventBus) AddSubscriber() chan ServerSentEvent {
+	ch := make(chan ServerSentEvent)
+	
+	bus.mutex.Lock()
+	bus.subscribers[ch] = true
+	bus.mutex.Unlock()
+
+	return ch
+}
+
+func (bus *EventBus) RemoveSubscriber(ch chan ServerSentEvent) {
+	bus.mutex.Lock()
+	delete(bus.subscribers, ch)
+	bus.mutex.Unlock()
+	close(ch)
+}
+
+func (bus *EventBus) Publish(id string, event EventType, data []byte) {
+
+	sse := ServerSentEvent{
+		Event: event,
+		Data: data,
+	}
+	
+	bus.mutex.Lock()
+	defer bus.mutex.Unlock()
+	for ch := range bus.subscribers {
+		ch <- sse
+	}
+}
+
+func (api *ApiV1) ListenEvents(w http.ResponseWriter, r *http.Request) {
+
+	ch := api.bus.AddSubscriber()
+	defer func() {
+		api.bus.RemoveSubscriber(ch)
+	}()
+	
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+	
+	flusher, ok := w.(http.Flusher)
+	if !ok {
+		http.Error(w, "Streaming not supported", http.StatusInternalServerError)
+		return
+	}
+
+	for {
+		select {
+		case sse := <-ch:
+			sse.Write(w)
+			flusher.Flush()
+		case <-r.Context().Done():
+			return
+		}
+	}
+}
